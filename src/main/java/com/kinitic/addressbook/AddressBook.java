3614783346fda36277b4bd4f66e55d4d0044bd6c
kinshuk.garai <kinshuk.garai@ext.soprasteria.com>
2017-03-10 15:32:11 +0000

    created failing test for counting number of males

---
+++ src/main/java/com/kinitic/addressbook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.kinitic.addressbook;
+
+public class AddressBook {
+
+    public long numberOfMales() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/kinitic/addressbook/AddressBookTest.java
@@ -0,0 +1,15 @@
+package com.kinitic.addressbook;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AddressBookTest {
+
+    @Test
+    public void shouldCountNumberOfMalesInAddressBook() {
+        final AddressBook addressBook = new AddressBook();
+        assertThat(addressBook.numberOfMales(), is(3L));
+    }
+}
